2c8f1832a5bffb9658dc8812788a5134f2b7896c
santhubairamcs <santhubairamcs@gmail.com>
2015-07-29 17:13:38 +0530

    #0008 | mocking View and Library and checking whether library given books passed to view

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -5,22 +5,16 @@ import java.util.ArrayList;
 public class BibliotecaApp {
     View view ;
     Library library;
-    ArrayList<String[]> books = new ArrayList<String[]>();
+    ArrayList<String[]> books;
 
-    public BibliotecaApp(View view){
+    public BibliotecaApp(View view, ArrayList<String[]> books){
         this.view = view;
-        String[] book1 = {"A Suitable Boy", "Vikram Seth ", "1993"};
-        String[] book2 = {"Cutting For Stone", "Abraham Verghese", "2009"};
-        String[] book3 = {"The White Tiger", "Aravind Adiga", "2008"};
-        String[] book4 = {"Train to Pakistan", "Khushwant Singh", "1956"};
-        books.add(book1);
-        books.add(book2);
-        books.add(book3);
-        books.add(book4);
+        this.books = books;
         library = new Library(books);
     }
 
     public void start() {
         view.greetUser();
+        view.listBooks(library.getBooks());
     }
 }
--- src/com/twu/biblioteca/Main.java
+++ src/com/twu/biblioteca/Main.java
@@ -1,10 +1,22 @@
 package com.twu.biblioteca;
 
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        ArrayList<String[]> books = new ArrayList<String[]>();
+        String[] book1 = {"A Suitable Boy", "Vikram Seth ", "1993"};
+        String[] book2 = {"Cutting For Stone", "Abraham Verghese", "2009"};
+        String[] book3 = {"The White Tiger", "Aravind Adiga", "2008"};
+        String[] book4 = {"Train to Pakistan", "Khushwant Singh", "1956"};
+        books.add(book1);
+        books.add(book2);
+        books.add(book3);
+        books.add(book4);
+        View view = new View();
+        BibliotecaApp bibliotecaApp = new BibliotecaApp(view , books);
         bibliotecaApp.start();
     }
 }

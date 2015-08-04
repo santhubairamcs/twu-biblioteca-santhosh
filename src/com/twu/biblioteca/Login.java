508bf0cde2790161c7a87e4fb1ccf83b8921e538
santhubairamcs <santhubairamcs@gmail.com>
2015-08-04 21:42:23 +0530

    #0063 | Storing list of users information and delegating authentication information handling invalid login

---
+++ src/com/twu/biblioteca/Login.java
@@ -0,0 +1,15 @@
+package com.twu.biblioteca;
+
+import java.util.ArrayList;
+
+
+public class Login {
+    ArrayList<User> users;
+
+    public Login(ArrayList<User> users) {
+        this.users = users;
+    }
+    public User authenticate(String libraryNumber, String password) {
+        return null;
+    }
+}
---
+++ test/com/twu/biblioteca/LoginTest.java
@@ -0,0 +1,22 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class LoginTest {
+
+    @Test
+    public void shouldKnowsHowToAuthenticateUserWhenWrongLibraryNumberAndPasswordGiven() {
+        ArrayList<User> users = new ArrayList<User>();
+        User user1 = new User("name1", "user1@gmail.com", "1234567890", "000-0001", "12345");
+        User user2 = new User("name2", "user2@gmail.com", "0987654321", "001-0001", "54321");
+        users.add(user1);
+        users.add(user2);
+        Login login = new Login(users);
+
+        assertEquals(null, login.authenticate("000-0001", "123"));
+    }
+}

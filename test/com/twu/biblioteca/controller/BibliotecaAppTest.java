09edf7f4343ca8973264ad4928e75707f8a1123c
santhubairamcs <santhubairamcs@gmail.com>
2015-08-07 19:20:36 +0530

    #0076 | checking whether logins authenticate fail condition

--- test/com/twu/biblioteca/controller/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/controller/BibliotecaAppTest.java
@@ -3,12 +3,8 @@ package com.twu.biblioteca.controller;
 import com.twu.biblioteca.model.*;
 import com.twu.biblioteca.view.View;
 import org.junit.Test;
-import org.mockito.internal.matchers.NotNull;
-
 import java.util.ArrayList;
 
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.notNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -25,5 +21,17 @@ public class BibliotecaAppTest {
         bibliotecaApp.userLogin();
         verify(loginMock).authenticate("1", "1");
      }
+
+    @Test
+    public void shouldKnowsWhatToDoWhenAuthenticationFail(){
+        View viewMock = mock(View.class);
+        Login loginMock = mock(Login.class);
+        BibliotecaApp bibliotecaApp = new BibliotecaApp(viewMock, new BookSection(new ArrayList<Book>()), new MovieSection(new ArrayList<Movie>()), loginMock);
+        when(viewMock.getUserInput()).thenReturn("1");
+        bibliotecaApp.userLogin();
+        verify(loginMock).authenticate("1", "1");
+        when(loginMock.authenticate("000-0001", "12345")).thenReturn(null);
+        verify(viewMock).outputConsole("Invalid login, Try again");
+    }
 
 }
